1e6f003e98496da3a59645e2be202ab012ed7a99
Noam Solan <noam5456@Noams-MacBook-Pro.local>
2018-06-15 13:23:10 +0300

    nesting

--- ex5/q1.asm
+++ ex5/q1.asm
@@ -1,39 +1,43 @@
 .model small
 .stack 100h
+
 .data
-out_msg db 'key 1 ','$'
+   out_msg db 'key 1 ','$'
 .code
-start:
+   Start:
+      ;change keyboard format
+      in al, 21h
+      ;save al value to restore keyboard format later
+      push ax
+      or al, 02h
+      out 21h, al
+      ;set data segment
+      mov ax, @data
+      mov ds, ax
 
-in al, 21h ;change keyboard format
-push ax ;save al value to restore keyboard format later
-or al, 02h
-out 21h, al
+      PollKeyboard:
+         ;check if a key was pressed
+         in al, 64h
+         test al, 01h
+         ;wait until a key will be pressed
+         jz PollKeyboard
+         ;check key values
+         in al, 60h
+         cmp al, 10000011b ;if the key is 2@ , escape
+         jz CON
 
-mov ax, @data ;set data segment
-mov ds, ax
+         in al, 60h ;check key value
+         cmp al,10000010b  ;if the key is 1! , print "key 1"
+         jnz PollKeyboard ;wait for the next press
 
-PollKeyboard:
-in al, 64h  ;check if a key was pressed
-test al, 01h
-jz PollKeyboard ;wait until a key will be pressed
+         mov dx, offset out_msg ;print "key 1" if you should
+         mov ah,9
+         int 21h
+         jmp PollKeyboard
 
-in al, 60h ;check key value
-cmp al, 10000011b ;if the key is 2@ , escape
-jz CON
-
-in al, 60h ;check key value
-cmp al,10000010b  ;if the key is 1! , print "key 1"
-jnz PollKeyboard ;wait for the next press
-
-mov dx, offset out_msg ;print "key 1" if you should
-mov ah,9
-int 21h
-jmp PollKeyboard
-
-CON:
-pop ax ;restore keyboard settings
-out 21h, al
-mov ah, 4ch
-int 21h
-end start
+      CON:
+         pop ax ;restore keyboard settings
+         out 21h, al
+         mov ah, 4ch
+         int 21h
+end Start
--- ex5/q21.asm
+++ ex5/q21.asm
@@ -4,31 +4,31 @@
 out_msg db '* ','$'
 
 .code
-HERE:
-mov ax, @data ;set data segment
-mov ds, ax
-mov bx, 05d
-mov cx, 0
+   Start:
+      mov ax, @data ;set data segment
+      mov ds, ax
+      mov bx, 05d
+      mov cx, 0
 
-PollClock:
-mov ax, 0
-mov al, 00h
-out 70h, al
-in al, 71h
-div bl
-cmp ah, 0
-jz PRINT
-jmp PollClock
+   PollClock:
+      mov ax, 0
+      mov al, 00h
+      out 70h, al
+      in al, 71h
+      div bl
+      cmp ah, 0
+      jz PRINT
+      jmp PollClock
 
-PRINT:
-cmp ax, cx
-jz PollClock
-mov cx, ax
-mov dx, offset out_msg ;print "*" if you should
-mov ah,9
-int 21h
-jmp PollClock
+   PRINT:
+      cmp ax, cx
+      jz PollClock
+      mov cx, ax
+      mov dx, offset out_msg ;print "*" if you should
+      mov ah,9
+      int 21h
+      jmp PollClock
 
 mov ah,4ch
 int 21h
-end HERE
+end Start
