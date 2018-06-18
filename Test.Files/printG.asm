bcc483964ece72bb5e184a53640d01ea371fff6a
Noam Solan <noam5456@Noams-MacBook-Pro.local>
2018-06-18 18:11:30 +0300

    working print to graphic

--- Test.Files/printG.asm
+++ Test.Files/printG.asm
@@ -1,1 +1,32 @@
-printG
+.model small
+
+.data
+   public txtGreen
+   txtGreen db 'GREEN*$'
+   txtBlue db 'BLUE@$'
+   txtWhite db 'WHITE#$'
+   txtExit db 'EXIT$'
+.code
+Main:
+   mov ax, @data
+   mov ds, ax
+   mov ah,00h
+   mov al,13h
+   int 10h
+   mov dh, 0
+   mov dl, 34
+   mov bh, 0
+   mov ah, 2
+   int 10h
+   mov ah, 09h
+   mov dx, offset txtGreen
+   int 21h
+   InfLoop:
+      ;jmp InfLoop
+   InfLoopEnd:
+   ;mov ah, 00h
+   ;mov al, 03h
+   ;int 10h
+   mov ah, 04ch
+   int 21h
+end Main
