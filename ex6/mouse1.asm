261b150a6dfdbe0287c9a80c23efec399ed27dd1
Ronen <shvoonder@gmail.com>
2018-07-03 17:13:26 +0300

    Final Final

--- ex6/mouse1.asm
+++ ex6/mouse1.asm
@@ -1,6 +1,7 @@
 .model small
 .stack 100h
 .data
+   ;declare data intergers as public
    public txtColors
    public txtExit
    public curColor
@@ -23,6 +24,7 @@
 
 
 .code
+   ;declare functions from the other file
    extern textsPrint:near
    extern mouseFollow:near
    extern setToVideo:near
@@ -30,15 +32,21 @@
    extern changeCurPos:near
    extern prtAtCursor:near
 
+   ;main
    Main:
+      ;set ds as data segment
       mov ax, @data
       mov ds, ax
       ;setting video mode to 256
       call setToVideo
       runTime:
+         ;print the line above
          call textsPrint
+         ;print the current pos of mouse
          call mouseFollow
+         ;check the current pos relative to the buttons
          call chkMousePos
+         ;loop
          jmp runTime
       runTimeEnd:
    end Main
--- ex6/mouse2.asm
+++ ex6/mouse2.asm
@@ -26,37 +26,37 @@
    ;dx - row pos of curser
    mouseFollow proc near uses ax
       mov ax, 03h
-      int 033h
-      shr cx, 1
+      int 033h ;get mouse position and button status
+      shr cx, 1 ;that's how the interrupt works
       ;pixel color change
-      mov al, curColor
+      mov al, curColor ;update the color of the cursor according to position
       mov ah, 0ch
-      int 10h
+      int 10h ;;Write graphics pixel at coordinate
       push cx
       push dx
-      shr dx, 1
+      shr dx, 1 ;divide dx and ax by 8 for the print of the symbol
       shr dx, 1
       shr dx, 1
       shr cx, 1
       shr cx, 1
       shr cx, 1
-      cmp prevMousePosC, cl
+      cmp prevMousePosC, cl ;check if cursot position changed
       jz contChkPos
       push bx
-      mov bl, prevMousePosC
+      mov bl, prevMousePosC ;move cursor to previous position
       mov newCurC, bl
       mov bl, prevMousePosD
       mov newCurD, bl
       mov bl, curShape
-      mov curShape, ' '
-      call changeCurPos
-      call prtAtCursor
+      mov curShape, ' ' ;delete the symbol that was printed at previous position
+      call changeCurPos ;function for changing position
+      call prtAtCursor ;function for printing
       mov curShape, bl
       pop bx
-      mov prevMousePosC, cl
+      mov prevMousePosC, cl ;updete previous position
       mov prevMousePosD, dl
       jmp contChkPosEnd
-      contChkPos:
+      contChkPos: ;same as before but for dl
         cmp prevMousePosD, dl
         jz contChkPosEnd
         ChangeD:
@@ -77,41 +77,41 @@
       contChkPosEnd:
       mov newCurC, cl
       mov newCurD, dl
-      call changeCurPos
-      call prtAtCursor
+      call changeCurPos ;go to current mosue posiotion
+      call prtAtCursor ;print symbol at current mouse position
       pop dx
       pop cx
       ret
    mouseFollow endp
-
+   ;check mouse position relative to the buttons and determaine
+   ;what the next curser should be, exit if needed
    chkMousePos proc near
       ;check if the mouse is in the upper row
       cmp dx, 07d
       ja chgToWhiteEnd
-      ;check if the mouse is in the "GREEN*" range
-      cmp cx, 048d
+      cmp cx, 048d   ;check if the mouse is in the "GREEN" range
       jb chgToGreen
-      cmp cx, 088d
+      cmp cx, 088d   ;check if the mouse is in the "BLUE" range
       jb chgToBlue
-      cmp cx, 0136d
+      cmp cx, 0136d   ;check if the mouse is in the "WHITE" range
       jb chgToWhite
-      cmp cx, 0287d
+      cmp cx, 0287d   ;check if the mouse is in the "EXIT" range
       ja exitP
-      jmp chgToWhiteEnd
-      exitP:
+      jmp chgToWhiteEnd ;don't exit program
+      exitP: ;exit program
          call exitProg
       exitPEnd:
-      chgToGreen:
+      chgToGreen: ;change to green function
           mov curColor, 0010b
           mov curShape, '*'
           jmp chgToWhiteEnd
       chgToGreenEnd:
-      chgToBlue:
+      chgToBlue: ;change to blue function
           mov curColor, 0001b
           mov curShape, '@'
           jmp chgToWhiteEnd
        chgToBlueEnd:
-       chgToWhite:
+       chgToWhite: ;change to white function
           mov curColor, 1111b
           mov curShape, '#'
           jmp chgToWhiteEnd
@@ -119,28 +119,32 @@
           ret
    chkMousePos endp
 
-   exitProg proc near
+   ;exit the program
+   exitProg proc near ;exit program function
       call setToNormal
       mov ah, 04ch
       int 21h
       ret
    exitProg endp
 
-   setToVideo proc near uses ax
+   ;set video mod to graphic
+   setToVideo proc near uses ax ;set graphic mode function
       mov ah,00h
       mov al,13h
       int 10h
       ret
    setToVideo endp
 
-   setToNormal proc near uses ax
+   ;set video mod to text§
+   setToNormal proc near uses ax ;exit graphic mode function
       mov ah, 00h
       mov al, 03h
       int 10h
       ret
    setToNormal endp
 
-   textsPrint proc near uses dx
+   ;print the text in the buttons
+   textsPrint proc near uses dx ;print color labels and 'exit' label
       ;setting init curser
       xor bh, bh
       xor dx, dx
@@ -150,7 +154,6 @@
       mov dx, offset txtColors
       mov ah, 09h
       int 21h
-
       ;cursor for right text
       xor dh, dh
       mov dl, 036d
@@ -163,7 +166,7 @@
       ret
    textsPrint endp
 
-   ;change cursor position to mouse position
+   ;change cursor position given posiiton
    changeCurPos proc near uses bx dx ax
       mov bh, 0h
       mov dh, newCurD
@@ -173,7 +176,7 @@
       ret
     changeCurPos endp
 
-    ;print at cursor position
+    ;print given symbol at given position
     prtAtCursor proc near uses ax cx bx
       mov ah, 09h
       mov al, curShape
